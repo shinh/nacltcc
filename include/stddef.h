9e7730dd51f78517524289ed021a09c94e014a6c
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2011-10-02 02:28:15 +0900

    stddef: ignore __thread

--- include/stddef.h
+++ include/stddef.h
@@ -23,6 +23,8 @@ typedef unsigned int wint_t;
 
 #define NULL ((void *)0)
 #define offsetof(type, field) ((size_t)&((type *)0)->field)
+/* XXX: support TLS? */
+#define __thread
 
 void *alloca(size_t size);
 
