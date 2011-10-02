0554abee93a68947a9e8a69c0d85d839cd23e308
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2011-10-02 18:09:56 +0900

    [NaCl] add data packer

---
+++ demo/pack_data.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+#
+# Pack data for NaCl tcc web interface
+#
+#  Copyright (c) 2011 Shinichiro Hamaji
+#
+# Usage:
+#
+# % ./pack_data.sh <path-to-your-simple_tar.py>
+#
+# Note that you should be able to find simple_tar.py in
+#
+# path_to/naclports/src/libraries/nacl-mounts/util/simple_tar.py
+
+if [ ! -x $1 ]; then
+    echo "Usage: $0 <path-to-your-simple_tar.py>"
+fi
+
+export FETCH_ENVS_ONLY=1
+. ../nacl-configure
+
+set -x
+
+rm -fr usr data.sar
+
+mkdir -p usr
+cp -r $NACL_INCLUDE_DIR usr
+rm -fr usr/include/c++
+
+mkdir -p usr/lib/tcc
+cp -r ../include usr/lib/tcc
+
+$1 usr data.sar
+
+echo DONE!
